6ef4c84cf53c86b50ab2856d51161d933c67edc0
manikantapmk <yadalapurapu.manikanta93@gmail.com>
2024-12-02 17:43:11 +0530

    second nav bar created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,13 @@ import "./index.css";
 import Layout from "./layout/Layout.jsx";
 import Header from "./layout/Header.jsx";
 import Footer from "./layout/Footer.jsx";
+import SecondNav from "./components/SecondNav.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Layout Header={<Header />} Footer={<Footer />}>
       <div className="container">
-        <h2>content start</h2>
+        <SecondNav />
       </div>
     </Layout>
   </>
